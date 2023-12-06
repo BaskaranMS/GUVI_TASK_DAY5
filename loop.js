03066661cb19ecd44ad723eddf91ae44d83384d0
BaskaranMS <official.ms.baskaran@gmail.com>
2023-12-06 16:56:00 +0530

    task-5 with output

--- loop.js
+++ loop.js
@@ -48,7 +48,7 @@ const resume = {
       "English (Fluent)",
       "Your Native Language (if different)"
     ],
-    "extra-curricular-activities": {
+    "extraCurricularActivities": {
       "discipline": "cricket",
       "representation": "district level",
       "teams": ["thanjavur district cricket association", "rvmcc", "pits college"]
@@ -63,7 +63,7 @@ const resume = {
   };
   
 //iterating the json
-
+console.log('QUESTION 1')
 //1.for-loop
 
 for (let key in resume) {
@@ -71,7 +71,18 @@ for (let key in resume) {
     console.log(`${key}: ${JSON.stringify(resume[key])}`);
   }
 }
+// OUTPUT:
+// profile: {"name":"BASKARAN M S","contact":{"email":"baskaran.ms.official@gmail.com","phone":6385881004,"address":"plot no 5, raja rajan nagar second street nanjikottai road","city":"Thanjavur","state":"Tamil Nadu","country":"India","postal code":613006}}
+// education: [{"degree":"higher secondary","school":"don bosco matric higher secondary school","year":2021},{"degree":"Bachelor of Engineering in Computer Science Engineering","school":"parisutham Institute of technology and science","year":2025}]
+// skills: ["MERN Stack Development","JavaScript","React","Node.js","Express.js","MongoDB","HTML","CSS","Git"]
+// experience: [{"position":"sales boy in bakery","year":"2020 - 2022"}]
+// working-projects: {"title":"college Website","description":"Developing a full-stack website using the MERN stack."}
+// languages: ["English (Fluent)","Your Native Language (if different)"]
+// extraCurricularActivities: {"discipline":"cricket","representation":"district level","teams":["thanjavur district cricket association","rvmcc","pits college"]}
+// interest: ["cricket","football","music","games"]
+// hobby: "travelling"
 
+console.log('QUESTION 2')
 //2.for...in loop
 
 for (let key in resume) {
@@ -79,7 +90,18 @@ for (let key in resume) {
       console.log(`${key}: ${JSON.stringify(resume[key])}`);
     }
   }
+// OUTPUT:
+// profile: {"name":"BASKARAN M S","contact":{"email":"baskaran.ms.official@gmail.com","phone":6385881004,"address":"plot no 5, raja rajan nagar second street nanjikottai road","city":"Thanjavur","state":"Tamil Nadu","country":"India","postal code":613006}}
+// education: [{"degree":"higher secondary","school":"don bosco matric higher secondary school","year":2021},{"degree":"Bachelor of Engineering in Computer Science Engineering","school":"parisutham Institute of technology and science","year":2025}]
+// skills: ["MERN Stack Development","JavaScript","React","Node.js","Express.js","MongoDB","HTML","CSS","Git"]
+// experience: [{"position":"sales boy in bakery","year":"2020 - 2022"}]
+// working-projects: {"title":"college Website","description":"Developing a full-stack website using the MERN stack."}
+// languages: ["English (Fluent)","Your Native Language (if different)"]
+// extraCurricularActivities: {"discipline":"cricket","representation":"district level","teams":["thanjavur district cricket association","rvmcc","pits college"]}
+// interest: ["cricket","football","music","games"]
+// hobby: "travelling"
 
+  console.log('QUESTION 3')
   //3.for...of loop
 
   for (let education of resume.education) {
@@ -105,8 +127,30 @@ for (let key in resume) {
   for (let interest of resume.interest) {
     console.log(`Interest: ${interest}`);
   }
-
+// OUTPUT:
+// Degree: higher secondary, School: don bosco matric higher secondary school, Year: 2021
+// Degree: Bachelor of Engineering in Computer Science Engineering, School: parisutham Institute of technology and science, Year: 2025
+// Skill: MERN Stack Development
+// Skill: JavaScript
+// Skill: React
+// Skill: Node.js
+// Skill: Express.js
+// Skill: MongoDB
+// Skill: HTML
+// Skill: CSS
+// Skill: Git
+// Position: sales boy in bakery, Year: 2020 - 2022
+// Language: English (Fluent)
+// Language: Your Native Language (if different)
+// Team: thanjavur district cricket association
+// Team: rvmcc
+// Team: pits college
+// Interest: cricket
+// Interest: football
+// Interest: music
+// Interest: games
   
+  console.log('QUESTION 4')
   //4.for...each loop
 
   resume.education.forEach(education => {
@@ -125,11 +169,33 @@ for (let key in resume) {
     console.log(`Language: ${language}`);
   });
   
-  resume.extra-curricular-activities.teams.forEach(team => {
+  resume.extraCurricularActivities.teams.forEach(team => {
     console.log(`Team: ${team}`);
   });
   
   resume.interest.forEach(interest => {
     console.log(`Interest: ${interest}`);
   });
-  
+
+// OUTPUT:
+// Degree: higher secondary, School: don bosco matric higher secondary school, Year: 2021
+// Degree: Bachelor of Engineering in Computer Science Engineering, School: parisutham Institute of technology and science, Year: 2025
+// Skill: MERN Stack Development
+// Skill: JavaScript
+// Skill: React
+// Skill: Node.js
+// Skill: Express.js
+// Skill: MongoDB
+// Skill: HTML
+// Skill: CSS
+// Skill: Git
+// Position: sales boy in bakery, Year: 2020 - 2022
+// Language: English (Fluent)
+// Language: Your Native Language (if different)
+// Team: thanjavur district cricket association
+// Team: rvmcc
+// Team: pits college
+// Interest: cricket
+// Interest: football
+// Interest: music
+// Interest: games
